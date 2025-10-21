3df2f091bb090ded1469c7bdac7e13ffc0d8f9c0
Ryutaro0514 <ryuryu_758_3602@icloud.com>
2025-10-22 00:39:34 +0900

    aaa

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 //全体をリロードしてリセット
-const btnreset=document.querySelector(".res")
-btnreset.addEventListener("click",()=>{
+const btnreset = document.querySelector(".res")
+btnreset.addEventListener("click", () => {
     location.reload()
 })
 //名前Aを確定
-const addA=document.querySelector(".addA")
-const playerA=document.querySelector(".playerA")
-const inputA=document.querySelector(".inputA")
-addA.addEventListener("click",()=>{
-    const currentA=inputA.value
+const addA = document.querySelector(".addA")
+const playerA = document.querySelector(".playerA")
+const inputA = document.querySelector(".inputA")
+addA.addEventListener("click", () => {
+    const currentA = inputA.value
     //ここで名前がないと入力前に返す
-    if(currentA){
-        addA.style.display="none"
-        playerA.textContent=currentA
+    if (currentA) {
+        addA.style.display = "none"
+        playerA.textContent = currentA
     }
 })
 //名前Bを確定
-const addB=document.querySelector(".addB")
-const playerB=document.querySelector(".playerB")
-const inputB=document.querySelector(".inputB")
-addB.addEventListener("click",()=>{
-    const currentB=inputB.value
+const addB = document.querySelector(".addB")
+const playerB = document.querySelector(".playerB")
+const inputB = document.querySelector(".inputB")
+addB.addEventListener("click", () => {
+    const currentB = inputB.value
     //ここで名前がないと入力前に返す
-    if(currentB){
-        addB.style.display="none"
-        playerB.textContent=currentB
+    if (currentB) {
+        addB.style.display = "none"
+        playerB.textContent = currentB
     }
 })
